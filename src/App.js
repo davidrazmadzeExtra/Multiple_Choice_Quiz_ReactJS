0974b4f81674bd2b4c350279db0b3fac0d41c600
David Razmadze <razd45@gmail.com>
2022-01-19 17:01:22 -0500

    Create hardcoded UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-	<h1>Capital Quiz</h1>
+      {/* 1. Header  */}
+      <h1>Capital Quiz</h1>
+
+      {/* 2. Current Score  */}
+      <h2>Score: 3</h2>
+
+      {/* 3. Question Card  */}
+      <div className="question-card">
+        {/* Current Question  */}
+        <h2>Question: 1 out of 5</h2>
+        <h3>What is the capital of America?</h3>
+
+        <ul>
+          <li>Boston</li>
+          <li>Miami</li>
+          <li>Washington DC</li>
+          <li>Dallas</li>
+        </ul>
+
+      </div>
+
     </div>
   );
 }
