94d989868d25fa0c9d8eee5e66ec7343f25a1d37
David Razmadze <razd45@gmail.com>
2022-01-19 17:41:03 -0500

    Show and hide final results card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // Properties
+  const [showResults, setShowResults] = useState(false);
+
   return (
     <div className="App">
       {/* 1. Header  */}
@@ -9,29 +13,29 @@ function App() {
       {/* 2. Current Score  */}
       <h2>Score: 3</h2>
 
-      {/* 3. Question Card  */}
-      <div className="question-card">
-        {/* Current Question  */}
-        <h2>Question: 1 out of 5</h2>
-        <h3 className="question-text">What is the capital of America?</h3>
+      {showResults ? (
+        /* 4. Final Results */
+        <div className="final-results">
+          <h1>Final Results</h1>
+          <h2>1 out of 5 correct - (20%)</h2>
+          <button>Restart game</button>
+        </div>
+      ) : (
+        /* 3. Question Card  */
+        <div className="question-card">
+          {/* Current Question  */}
+          <h2>Question: 1 out of 5</h2>
+          <h3 className="question-text">What is the capital of America?</h3>
 
-        {/* List of possible answers  */}
-        <ul>
-          <li>Boston</li>
-          <li>Miami</li>
-          <li>Washington DC</li>
-          <li>Dallas</li>
-        </ul>
-
-      </div>
-
-      {/* 4. Final Results */}
-      <div className="final-results">
-        <h1>Final Results</h1>
-        <h2>1 out of 5 correct - (20%)</h2>
-        <button>Restart game</button>
-      </div>
-
+          {/* List of possible answers  */}
+          <ul>
+            <li onClick={() => setShowResults(true)}>Boston</li>
+            <li>Miami</li>
+            <li>Washington DC</li>
+            <li>Dallas</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
